bded65dc3d3efc7787ebb8402aee27c25cc90daa
Nathan Dubocage <nathandubocage@gmail.com>
2023-04-30 15:03:09 +0200

    Ajout d'un bouton pour tester la reconstruction de la carte

--- lib/views/map/map.dart
+++ lib/views/map/map.dart
@@ -9,6 +9,7 @@ class GoogleMapView extends StatefulWidget {
 }
 
 class _GoogleMapViewState extends State<GoogleMapView> {
+  MapType currentMapType = MapType.normal;
   late GoogleMapController controller;
 
   final CameraPosition initialPosition = const CameraPosition(
@@ -16,18 +17,44 @@ class _GoogleMapViewState extends State<GoogleMapView> {
     zoom: 11.0,
   );
 
+  void onMapTypeButtonPressed() {
+    setState(() {
+      currentMapType =
+          currentMapType == MapType.normal ? MapType.satellite : MapType.normal;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Google Map'),
       ),
-      body: GoogleMap(
-        initialCameraPosition: initialPosition,
-        onMapCreated: (GoogleMapController controller) {
-          controller = controller;
-        },
-        mapType: MapType.normal,
+      body: Stack(
+        children: [
+          GoogleMap(
+            initialCameraPosition: initialPosition,
+            onMapCreated: (GoogleMapController controller) {
+              controller = controller;
+            },
+            mapType: currentMapType,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Column(
+                children: [
+                  FloatingActionButton(
+                    onPressed: onMapTypeButtonPressed,
+                    materialTapTargetSize: MaterialTapTargetSize.padded,
+                    child: const Icon(Icons.map, size: 36.0),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
